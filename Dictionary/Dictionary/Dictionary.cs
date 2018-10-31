0c30e6d67927e3a52133af7684d81784d0b92df9
HorskayaAnna <Horskaya.Anna@gmail.com>
2018-10-31 18:50:41 +0300

    add task

---
+++ Dictionary/Dictionary/Dictionary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Dictionary
+{
+    class Dictionaryy<U, T>
+    {
+        public U[] Key { get; set; }
+        public T[] Value { get; set; }
+        public int Count { get; set; }
+
+        public Dictionaryy(int capasity)
+        {
+            Key = new U[capasity];
+            Value = new T[capasity];
+            Count = 0;
+        }
+
+        public void Add(U paramKey, T paramValue)
+        {
+            if (!Key.Contains(paramKey))
+            {
+                Key[Count] = paramKey;
+                Value[Count] = paramValue;
+                Count++;
+            }
+        }
+
+        public void Delete(U paramKey)
+        {
+            if (!Key.Contains(paramKey))
+            {
+                int index = Array.IndexOf(Key, paramKey);
+                Array.Copy(Key, index + 1, Key, index, Count - index - 2);
+                Array.Copy(Value, index + 1, Value, index, Count - index - 2);
+                Count--;
+            }
+        }
+
+        public void Update(U paramKey, T paramValue)
+        {
+            if (Key.Contains(paramKey))
+            {
+                Value[Array.IndexOf(Key, paramKey)] = paramValue;
+            }
+        }
+    }
+}
---
+++ Dictionary/Dictionary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionaryy<int, int> dict= new Dictionaryy<int, int>(5);
+            dict.Add(1, 2);
+            dict.Add(2, 2);
+            dict.Add(3, 2);
+            dict.Add(4, 2);
+            dict.Add(1, 2);
+            dict.Update(2, 5);
+            dict.Delete(2);
+            for (int i = 0; i < dict.Count; i++)
+            {
+                Console.WriteLine($"{dict.Key[i]}, {dict.Value[i]}");
+            }
+        }
+    }
+}
